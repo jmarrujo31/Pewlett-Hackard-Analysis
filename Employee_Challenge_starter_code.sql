218d46ee0df451cd151bf3af70f4b0da6d1737ed
jmarrujo31 <89328181+jmarrujo31@users.noreply.github.com>
2021-10-07 19:25:47 -0700

    t

--- Employee_Challenge_starter_code.sql
+++ Employee_Challenge_starter_code.sql
@@ -1,9 +1,20 @@
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+SELECT employees.emp_no, employees.first_name, employees.last_name, titles.title, titles.from_date, titles.to_date
+INTO retirement_titles
+FROM employees
+LEFT JOIN titles
+ON employees.emp_no = titles.emp_no
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
 
-INTO nameyourtable
-FROM _______
-ORDER BY _____, _____ DESC;
+
+SELECT DISTINCT ON (titles.emp_no) employees.emp_no, employees.first_name, employees.last_name, titles.title
+INTO unique_titles
+FROM employees
+LEFT JOIN titles
+ON employees.emp_no = titles.emp_no
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+
+SELECT COUNT(unique_titles.title), title
+FROM unique_titles
+GROUP BY unique_titles.title
+ORDER BY title;
