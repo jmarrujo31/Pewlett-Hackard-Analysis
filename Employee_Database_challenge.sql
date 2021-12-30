732bee6f13b66c0b5038a04c04fcfb6285408c03
jmarrujo31 <89328181+jmarrujo31@users.noreply.github.com>
2021-12-29 18:58:47 -0800

    l

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,26 +1,23 @@
+--Deliverable 1 Requirements
 SELECT employees.emp_no, employees.first_name, employees.last_name, titles.title, titles.from_date, titles.to_date
 INTO retirement_titles
 FROM employees
 LEFT JOIN titles
 ON employees.emp_no = titles.emp_no
-WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
-
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+ORDER BY emp_no;
 -- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (employees.emp_no) employees.emp_no, employees.first_name, employees.last_name, titles.title
+SELECT DISTINCT ON (retirement_titles.emp_no) retirement_titles.emp_no, retirement_titles.first_name, retirement_titles.last_name, retirement_titles.title
 INTO unique_titles
-FROM employees
-LEFT JOIN titles
-ON employees.emp_no = titles.emp_no
-WHERE to_date = '9999-01-01'
-and birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+FROM retirement_titles
 ORDER BY emp_no ASC,to_date DESC;
-
+--retiring_titles
 SELECT COUNT(unique_titles.title), title
 INTO retiring_titles
 FROM unique_titles
 GROUP BY unique_titles.title
 ORDER BY COUNT DESC;
-
+--Deliverable 2 Requirements
 SELECT DISTINCT ON (emp_no) employees.emp_no, employees.first_name, employees.last_name, employees.birth_date, dept_emp.from_date, dept_emp.to_date, titles.title
 INTO mentorship_eligibility
 FROM employees
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# Pewlett-Hackard-Analysis
+# Pewlett-Hackard-Analysis 
+### Employees “silver tsunami” Retirement Analysis
+-
+
+Overview
+-
+Load in Data from CSV file into SQL Database
+
+Search SQL DB for Retirement eligibility
+
+Results
+-
+
+Retirement by Department
+
+![](Resources/unique_titles.png)
+
+Total Retirement eligibility =  **90,398** employees
+
+Total Mentorship eligibilty = **1,549** employees
+
+Summary
+-
+
+Reviewing the data, it appears that we should start to recruit more technical personal for our engineering staff.
+
+All data, CSV files are located in the Data Folder.
+
+
+
+
